39f0a7e2b3d88dea0e04233a246be62bf4e50273
Kevin Rodrigues <kevin_3467@yahoo.com>
2026-02-22 10:40:54 -0300

    feat: add particles to the game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,33 +1,64 @@
 extends RigidBody3D
 
-# Magnitude da força de movimentação do player
 @export_range(100, 3000) var movement_mag: float = 1000
-# Magnitude da força de rotação do player
 @export_range(100, 3000) var rotation_mag: float = 500
+@onready var death_sfx: AudioStreamPlayer = $DeathSFX
+@onready var success_sfx: AudioStreamPlayer = $SuccessSFX
+@onready var thrust_sfx: AudioStreamPlayer3D = $ThrustSFX
+
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+@onready var explosion_particles: GPUParticles3D = $ExplosionParticles
+@onready var success_particles: GPUParticles3D = $SuccessParticles
 
 var is_transitioning: bool = false
 
+
 func boost(delta):
+	booster_particles.emitting = true
+	right_booster_particles.emitting = true
+	left_booster_particles.emitting = true
+	if !thrust_sfx.playing:
+			thrust_sfx.play()
 	apply_central_force(basis.y * delta * movement_mag)
+ 
+func stop_boost():
+	thrust_sfx.stop()
+	booster_particles.emitting = false
+	right_booster_particles.emitting = false
+	left_booster_particles.emitting = false
 
 func rotate_right(delta):
 	apply_torque(Vector3.FORWARD * delta * rotation_mag)
+	left_booster_particles.emitting = false
 
 func rotate_left(delta):
 	apply_torque(Vector3.BACK * delta * rotation_mag)
+	right_booster_particles.emitting = false
 
 func crash_sequence() -> void:
 	is_transitioning = true
-	print('Kaboom!')
+	death_sfx.play()
+	thrust_sfx.stop()
+	booster_particles.emitting = false
+	right_booster_particles.emitting = false
+	left_booster_particles.emitting = false 
+	explosion_particles.emitting = true
 	var tween = create_tween()
-	tween.tween_interval(1)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 	set_process(false)
 	
 func complete_level(next_level_file_path: String) -> void:
 	is_transitioning = true
+	success_sfx.play()
+	booster_particles.emitting = false
+	right_booster_particles.emitting = false
+	left_booster_particles.emitting = false
+	success_particles.emitting = true
 	var tween = create_tween()
-	tween.tween_interval(1)
+	tween.tween_interval(2.5)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file_path)
 	)
@@ -40,13 +71,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_pressed('boost'):
 		boost(delta)
+	else:
+		stop_boost()	
 	
 	if Input.is_action_pressed('rotate_right'):
 		rotate_right(delta)
 	
 	if Input.is_action_pressed('rotate_left'):
 		rotate_left(delta)
-
 
 func _on_body_entered(body: Node) -> void:
 	if !is_transitioning:
